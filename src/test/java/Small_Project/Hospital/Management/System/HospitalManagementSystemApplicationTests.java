2ff25a785f23d99c22627c1faef98175c7335017
POMMOUYLANG <mouylangpom@gamail.com>
2024-02-26 22:07:56 +0700

    HMS

---
+++ src/test/java/Small_Project/Hospital/Management/System/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Small_Project.Hospital.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
